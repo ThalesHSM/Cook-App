71bf3f290e10278faaad9ec32ac1b4c2ee23ebdf
ThalesHSM <thsm271@gmail.com>
2024-03-05 22:34:53 -0300

    Finished Cook app

---
+++ src/services/index.ts
@@ -0,0 +1,14 @@
+import * as recipes from "./recipesService";
+import * as ingredientes from "./ingredientsService";
+import * as preparations from "./preparationsService";
+
+export const services = {
+  recipes,
+  ingredientes,
+  preparations,
+
+  storage: {
+    imagePath:
+      "https://bjedyscdlvrsgflimrgm.supabase.co/storage/v1/object/public/ingredients",
+  },
+};
